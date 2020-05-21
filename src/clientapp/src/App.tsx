01a1d971743e448bc1c08c9a34b882108b098e47
Jason <jason.silvers@outlook.com>
2020-05-21 12:46:56 -0500

    add routing

--- src/clientapp/src/App.tsx
+++ src/clientapp/src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Redirect, Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
 import './App.css';
 import {AppProvider} from "./context/AppContext";
 import Hello from "./components/Hello";
 import TeachersList from "./components/TeachersList";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
     return (
         <AppProvider>
-            <Hello />
-            <TeachersList />
+            <Router basename={process.env.PUBLIC_URL}>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact={true} component={Hello} />
+                    <Route path="/teachers" exact={true} component={TeachersList} />
+                </Switch>
+            </Router>
+
+
         </AppProvider>
     );
 }
